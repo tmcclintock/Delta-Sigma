5972ee58faec7457f0b92097e3a661b659294606
Tom McClintock <tmcclintock@email.arizona.edu>
2016-06-03 12:00:26 -0700

    Currently writing the python wrapper.

--- pymain.py
+++ pymain.py
@@ -2,3 +2,25 @@
 This is an example file of how to interface with the python wrapper
 that can run the delta sigma code.
 """
+
+import sys
+sys.path.insert(0,"src/wrapper/")
+import py_Delta_Sigma
+
+import numpy as np
+
+#First load in the power spectrum data
+klin = np.genfromtxt("test_data/matter_power_lin/k_h.txt")
+Plin = np.genfromtxt("test_data/matter_power_lin/p_k.txt")
+knl = np.genfromtxt("test_data/matter_power_nl/k_h.txt")
+Pnl = np.genfromtxt("test_data/matter_power_nl/p_k.txt")
+
+Mass = 1.e14
+concentration = 4.0*(Mass/5e14)**-0.1
+NR = 300
+Rmin,Rmax = 0.01,200.0
+Nbins = 15
+R_bin_min, R_bin_max = 0.01,200.0
+delta = 200
+Rmis, fmis = 0.3,0.23
+
---
+++ src/wrapper/py_Delta_Sigma.py
@@ -0,0 +1,8 @@
+"""
+This is the python wrapper that calls the python_wrapper()
+c function. This interfaces through c_types so that the user
+doesn't have to.
+"""
+
+def calc_Delta_Sigma(klin,Plin,knl,Pnl):
+    
