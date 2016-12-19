0ed32a6d5bab7e751da4cd766cb699da567c18e2
Tom McClintock <tmcclintock@email.arizona.edu>
2016-12-19 15:35:33 +0100

    Made a function signature more pretty looking in a header.

--- src/miscentered_delta_sigma/miscentered_delta_sigma_at_r.h
+++ src/miscentered_delta_sigma/miscentered_delta_sigma_at_r.h
@@ -6,4 +6,10 @@
 #include <stdlib.h>
 #include <string.h>
 
-int calc_miscentered_delta_sigma_at_r(double Rp,double Mass,double concentration,int delta,double Rmis,double*R,double*sigma_r,double*mis_sigma_r,double sigma_less_rmin,int NR,double*mis_delta_sigma,double*err,cosmology cosmo);
+int calc_miscentered_delta_sigma_at_r(double Rp,double Mass,
+				      double concentration,int delta,
+				      double Rmis,double*R,double*sigma_r,
+				      double*mis_sigma_r,
+				      double sigma_less_rmin,int NR,
+				      double*mis_delta_sigma,
+				      double*err,cosmology cosmo);
