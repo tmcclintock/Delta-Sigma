d8ee3cffda59fe81d31e0466e4a139347a1dea13
Tom McClintock <tmcclintock@email.arizona.edu>
2017-05-23 20:24:14 -0700

    Re-making the main.c file so that I can test the python_interface function.

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+This is a main function that tests the wrapper::python_interface() function,
+in order to figure out if there are memory leaks.
+ */
+
+#include<stdio.h>
+#include<stdlib.h>
+#include"src/wrapper/wrapper.h"
+
+int main(){
+  printf("test main\n");
+  //TODO: allocate everything and pass it in to the function
+  return 0;
+}
