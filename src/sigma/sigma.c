ee35474f915f098c6c8c74ebb4c5bbb4074b09ec
Tom McClintock <tmcclintock89@gmail.com>
2017-06-17 11:11:53 -0500

    Removed OMP dependences from sigma.* files.

--- src/sigma/sigma.c
+++ src/sigma/sigma.c
@@ -4,10 +4,8 @@ int calc_sigma(double*Rp,double Mass,double concentration,
 	       int delta,double*R,double*xi,int NR,double*sigma,
 	       double*err,cosmology cosmo){
   int i,status=0;
-
-#pragma omp parallel for shared(R,xi,NR,sigma,err,status)
-    for(i = 0; i < NR; i++)
-      status |= calc_sigma_at_r(Rp[i],Mass,concentration,delta,R,xi,NR,&sigma[i],&err[i],cosmo);
-    
-    return status;
+  for(i = 0; i < NR; i++){
+    status |= calc_sigma_at_r(Rp[i],Mass,concentration,delta,R,xi,NR,&sigma[i],&err[i],cosmo);
+  }
+  return status;
 }
--- src/sigma/sigma.h
+++ src/sigma/sigma.h
@@ -1,7 +1,6 @@
 #include "sigma_at_r.h"
 #include "../cosmology/cosmology.h"
 #include "../xi_nfw/xi_nfw.h"
-#include <omp.h>
 
 int calc_sigma(double*Rp,double Mass,double concentration,
 	       int delta,double*R,double*xi,int NR,double*sigma,
