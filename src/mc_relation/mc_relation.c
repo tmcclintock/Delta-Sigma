5f834441ba3719f71858f230d3c1895e6bfc28a8
Tom McClintock <tmcclintock89@gmail.com>
2017-06-17 13:12:12 -0500

    Got rid of the mc_relation directory since I don't think i'll ever write it.

--- src/mc_relation/mc_relation.c
+++
@@ -1,9 +0,0 @@
-#include "mc_relation.h"
-
-int calc_mc_relation(double*nu,int Nnu,double*c,int delta,cosmology cosmo){
-  int i;
-  for(i = 0; i< Nnu; i++){
-    //mc_relation_at_nu(nu[i],&c[i],delta,cosmo);
-  }
-  return 0;
-}
--- src/mc_relation/mc_relation.h
+++
@@ -1,5 +0,0 @@
-//#include "mc_relation_at_nu.h"
-#include <omp.h>
-#include "../cosmology/cosmology.h"
-
-int calc_mc_relation(double*nu,int Nnu,double*c,int delta,cosmology cosmo);
