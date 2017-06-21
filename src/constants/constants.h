9ee87b2bd891892e33647d60ecf2425bdd90833a
Tom McClintock <tmcclintock89@gmail.com>
2017-06-20 19:15:27 -0700

    Fixed the rhomconst constant, which didn't affect the DeltaSigma miscentering at all, because it was almost never used in the calculation.

--- src/constants/constants.h
+++ src/constants/constants.h
@@ -19,7 +19,7 @@
 #define G 4.517e-48//Newton's G in Mpc^3/s^2/Solar Mass
 #define Mpcperkm 3.241e-20//Mpc/km used to convert H0 to per seconds
 #define delta_c 1.686 //Critical collapse density
-#define rhomconst 3.0162e11//1e4*3.*Mpcperkm*Mpcperkm/(8.*PI*G); units are SM h^2/Mpc^3
+#define rhomconst 2.775808e+11//1e4*3.*Mpcperkm*Mpcperkm/(8.*PI*G); units are SM h^2/Mpc^3
 #endif
 
 //Now define constants
