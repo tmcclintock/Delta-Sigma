8ab1878afec7f6314a51595655597edc6fdb6cfe
Tom McClintock <tmcclintock@email.arizona.edu>
2017-08-08 16:44:37 -0700

    Removed references to the tolerances used in miscentering rom the constants.h file.

--- src/constants/constants.h
+++ src/constants/constants.h
@@ -9,8 +9,6 @@
 #define TOL 1e-5
 #define AVE_TOL 1e-8 //Used for taking averages
 #define BIAS_TOL 1e-6 //Used for the tinker bias
-#define MISCENTERED_TOL 1e-2 // Used for miscentering
-#define MISCENTERED_TOL2 MISCENTERED_TOL*0.1
 #endif
 
 //Now define physical constants
